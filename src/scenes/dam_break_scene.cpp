fe40b7d4708334d4a20dd29276dfc6ad58ee1bec
Lui Habl <txaicalvoso@gmail.com>
2025-10-02 10:01:50 -0300

    Add compact block on dam break scene

--- src/scenes/dam_break_scene.cpp
+++ src/scenes/dam_break_scene.cpp
@@ -2,15 +2,18 @@
 
 #include "models/lague_model.h"
 #include "models/model.h"
+#include "simulation.h"
 
 namespace vfs {
 
 void DamBreakScene::Init() {
+    fluid_block_size = {50, 60, 50};
+
     auto base_parameters = SPHModel::Parameters{
         .time_scale = 1.0f,
         .fixed_dt = 1.0f / 120.0f,
         .iterations = 3,
-        .n_particles = 400000,
+        .n_particles = fluid_block_size.x * fluid_block_size.y * fluid_block_size.z,
         .target_density = 630.0f,
         .bounding_box = {.size{23.0f, 10.0f, 10.0f}},
     };
@@ -30,10 +33,17 @@ void DamBreakScene::Clear() {
 
 void DamBreakScene::Reset() {
     const auto box = time_step_model->GetBoundingBox().value();
-    auto size = box.size;
-    size.x /= 5.0f;
+    const auto n_particles = time_step_model->GetParameters().n_particles;
+    const auto density = time_step_model->GetParameters().target_density;
 
-    auto pos = box.pos;  // + (box.size - size) / 2.0f;
+    auto particle_volume = 1 / density;
+    auto particle_diam = std::cbrt(particle_volume);
+
+    glm::vec3 size = particle_diam * static_cast<glm::vec3>(fluid_block_size + 1);
+
+    auto pos = box.pos;
+    pos.z += (box.size.z - size.z) / 2.0f;
+
     time_step_model->SetParticlesInBox(gfx, {.size = size, .pos = pos});
 }
 
